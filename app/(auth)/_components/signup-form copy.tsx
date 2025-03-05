278410321ce6962f181280e38dd7027de6fe32fd
Callmesammy <callmesammy@yahoo.com>
2025-03-05 04:52:10 -0800

    update

---
+++ app/(auth)/_components/signup-form copy.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Please enter your correct password"
+  }),
+  confirm_password: z.string().min(6,{
+    message: "Password did not match"
+  })
+})
+
+export function SignUpForm() {
+     // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      confirm_password: "",
+      password: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+    return (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Enter Email" {...field} />
+                  </FormControl>
+                  
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirm_password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>confirm-password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">Signup</Button>
+          </form>
+        </Form>
+      )
+ 
+}
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,29 @@
+"use client"
+import Image from "next/image";
 import { FlipWor } from "../_components/flip-words";
+import { useState } from "react";
+import { LoginForm } from "../_components/login-form";
+import { SignUpForm } from "../_components/signup-form copy";
 
 
 const  Login = () => {
+    const [onPage, setOnPage]=useState("login");
+
     return ( 
         <div className="flex flex-col w-full h-screen items-center justify-center">
+            <div className="flex bg-white flex-col p-4 rounded justify-center items-center space-y-3">
             <FlipWor/>
           <div className="w-[25rem] h-[30rem] border rounded-md ">
-        Login here
-        </div>  
+       <h1 className="text-xl font-bold px-3 pt-8">
+        {onPage === "login" ? "Login" : "SignUp"}</h1>
+        <div className="px-4 pt-6 ">
+        {onPage === "login" && <div className="pt-6 flex flex-col h-full w-full "><LoginForm/> <p className="text-sm justify-end pt-3 text-end">Dont have an account ? <span  className="text-blue-700 cursor-pointer underline hover:font-bold" onClick={()=> setOnPage("signup")}>Signup</span></p></div>}
+        {onPage === "signup" &&  <div className="pt-6 flex flex-col h-full w-full "><SignUpForm/> <p className="text-sm justify-end pt-3 text-end">Already have an account ? <span  className="text-blue-700 cursor-pointer underline hover:font-bold" onClick={()=> setOnPage("login")}>Login</span></p></div>}
+        </div>
+        </div>  </div>
+        <div className="-z-10 flex shadow-md">
+            <Image src="/tod.webp" alt="image" fill className="object-cover" />
+        </div>
         </div>
      );
 }
