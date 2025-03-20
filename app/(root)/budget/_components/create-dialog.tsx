69c4d5bb78aa1a273f6af86ad9f9459ae264efbb
Callmesammy <callmesammy@yahoo.com>
2025-03-20 03:32:06 -0700

    update

--- app/(root)/budget/_components/create-dialog.tsx
+++ app/(root)/budget/_components/create-dialog.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -8,11 +8,11 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Loader2, Plus } from "lucide-react" 
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+} from "@/components/ui/dialog";
+import { Loader2, Plus } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
@@ -20,139 +20,176 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "sonner"
-import CreateTask from "../../actions/creat-actions"
-import { useState } from "react"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useState } from "react";
+import CreateTask from "../../actions/creat-actions";
+import EmojiPicker from "emoji-picker-react";
+import { useRouter } from "next/navigation";
 
-
- 
 export const formSchema = z.object({
-    title: z.string().min(2, {
-        message: "Please enter the title of your task"
-    }), 
-    description: z.string().min(2, {
-        message: "Enter Amount"
-    }), 
-    amountBudget: z.coerce.number().min(6, {
-      message: "Please enter and amount"
-    })
-  })
+  title: z.string().min(1, {
+    message: "Please enter the title of your task",
+  }),
+  description: z.string().min(2, {
+    message: "Enter Amount",
+  }),
+  amountSpent: z.coerce.number().min(6, {
+    message: "Please enter and amount",
+  }),
+});
 export function CreateDialog() {
+  const [emoji, setEmoji] = useState("😊");
+  const [enterEmoji, setEnterEmoji] = useState(false);
+  const router = useRouter()
 
   const [loading, setLoadin] = useState(false);
-  const [closePage, setClosePage] = useState(true)
-      // 1. Define your form.
+  const [closePage, setClosePage] = useState(true);
+  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
       description: "",
-      amountBudget: 0
+      amountSpent: 0,
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {    
-       
-
-    try{
-      const {success, error} = await CreateTask(values)
-      if(!success){ 
-        toast.error(String(error))
-      }else{
-        toast.success("created successfully")
-        setLoadin(true)
-        setClosePage(true)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const { success, error } = await CreateTask(values);
+      if (!success) {
+        toast.error(String(error));
+      } else {
+        toast.success("created successfully");
+        setLoadin(true);
+        setClosePage(true);
         form.reset();
-        
-      }
-
-
-    }catch(error){
-        console.log(error)
-    }finally{
-      setLoadin(false)
-      setClosePage(false)
+        router.refresh()
       
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadin(false);
+      setClosePage(false);
     }
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log( values); 
+     
+
   }
   return (
-  <>
-   
-     <Dialog open={closePage} onOpenChange={setClosePage}>
-      
-       <DialogTrigger asChild>
-      <Button className="border h-full text-black hover:text-white text-pretty hover:shadow-xl cursor-pointer rounded bg-secondary flex-col flex px-3 items-center justify-center text-sm">
-                <Plus/>
-       Create New Task
+    <>
+      <Dialog open={closePage} onOpenChange={setClosePage}>
+        <DialogTrigger asChild>
+          <Button className="border h-full text-black hover:text-white text-pretty hover:shadow-xl cursor-pointer rounded bg-secondary flex-col flex px-3 items-center justify-center text-sm">
+            <Plus />
+            Create New Task
+          </Button>
+        </DialogTrigger>
+        {closePage && (
+          <>
+            {" "}
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Create Your Task</DialogTitle>
+                <DialogDescription>
+                  Add New activities and click save when done
+                </DialogDescription>
+              </DialogHeader>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Emoji</FormLabel>
+                        <FormControl>
+                          <div>
+                            <Button
+                              className="cursor-pointer"
+                              onClick={() => setEnterEmoji(!enterEmoji)}
+                            >
+                              {field.value || emoji}
+                            </Button>
 
-              </Button>   
-      </DialogTrigger> 
-     {closePage && <> <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create Your Task</DialogTitle>
-          <DialogDescription>
-            Add New activities and click save when done 
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input  disabled={loading} placeholder="What do you want to do?" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                            {enterEmoji && (
+                              <div className="absolute z-50">
+                                <EmojiPicker
+                                  onEmojiClick={(e) => {
+                                    setEmoji(e.emoji);
+                                    field.onChange(e.emoji); // ✅ Update form value
+                                    setEnterEmoji(false); // setting of the emoji to disabled when click
+                                  }}
+                                />
+                              </div>
+                            )}
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-<FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Input  disabled={loading} placeholder="More about your task" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="amountBudget"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Amount</FormLabel>
-              <FormControl>
-                <Input type="number"  disabled={loading} placeholder="Input Amount" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button disabled={loading} type="submit" className="w-full cursor-pointer">Add Item 
-          {loading && <Loader2 className="animate-spin "/>}</Button>
-      </form>
-    </Form>  
-      </DialogContent></>}
-      
-            </Dialog>
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input
+                            disabled={loading}
+                            placeholder="More about your task"
+                            {...field}
+                          />
+                        </FormControl>
 
-            </>
-  )
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="amountSpent"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Amount</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            disabled={loading}
+                            placeholder="Input Amount"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button
+                    disabled={loading}
+                    type="submit"
+                    className="w-full cursor-pointer"
+                  >
+                    Add Item
+                    {loading && <Loader2 className="animate-spin " />}
+                  </Button>
+                </form>
+              </Form>
+            </DialogContent>
+          </>
+        )}
+      </Dialog>
+    </>
+  );
 }
--- app/(root)/budget/page.tsx
+++ app/(root)/budget/page.tsx
@@ -5,27 +5,28 @@ import { useEffect, useState } from "react";
 import { CreateDialog } from "./_components/create-dialog";
 
 interface withProps {
-  name: string;
-  item: number;
-  id: string;
+  amountSpent: number;
 }
 
 interface TaskProps {
-  title: string;
-  item: number;
+  amountSpent: number,
+  title: string,
+  description: string,
   id: string;
   dock: withProps[];
 }
 const Budget = () => {
   const [isSubmitting, setIsSubmitting] = useState<TaskProps[]>([]);
 
+
   useEffect(() => {
     const shown = async () => {
       const supabase = await createClient();
 
-      const { data, error } = await supabase.from("tasks").select("*");
+      const { data, error } = await supabase.from("flexy").select("*");
       if (data) {
         setIsSubmitting(data as TaskProps[]);
+        
       } else {
         console.log(error);
       }
@@ -36,17 +37,36 @@ const Budget = () => {
   return (
     <div className="px-2 pt-5 flex w-full h-full flex-col">
       <h1 className="text-pretty font-bold text-2xl">My Budgets</h1>
-      <div className="pt-5 grid md:grid-cols-2 lg:grid-cols-3 w-full  h-[15rem] gap-3">
+              <div className="w-full h-full flex ">
+
+      <div className="pt-5 grid md:grid-cols-2 lg:grid-cols-3 w-full p-3 h-[12rem] gap-3">
+
+       
         <CreateDialog />
 
         {isSubmitting.map((budget) => (
-          <div key={budget.id}>
-            {budget.title}
-            
+          <div key={budget.id} className="border w-full h-[12rem] flex">
+            <div className="flex justify-between px-3 pt-3 w-full items-center">
+              <div className="flex items-center space-x-3">
+              <div className="text-3xl gap-2  p-2 h-[3rem] rounded-full items-center flex bg-purple-400">{budget.title}
+              </div>
+              <h1 className="capitalize text-sm font-bold">
+
+                              {budget.description}
+
+              </h1>
+              </div>
+          <h1 className="text-lg font-bold text-pretty"> N{budget.amountSpent} </h1>
+
+
+            </div>
+            <div>
+
+            </div>
           </div>
         ))}
       </div>
-    </div>
+    </div> </div>
   );
 };
 
